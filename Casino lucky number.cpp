17d2e480c55c51e2061f99c3ec49520ad8060181
Kushrajpurohit <56964911+Kushrajpurohit@users.noreply.github.com>
2021-09-09 11:24:29 +0530

    Update Casino lucky number.cpp

--- Casino lucky number.cpp
+++ Casino lucky number.cpp
@@ -1,38 +1,36 @@
 #include <iostream>
-#include <string> // Needed to use strings
-#include <cstdlib> // Needed to use random numbers
+#include <string> 
+#include <cstdlib> 
 #include <ctime>
 using namespace std;
 void rules();
 int main()
 {
     string playerName;
-    int balance; // stores player's balance
+    int balance;
     int bettingAmount;
     int guess;
-    int dice; // stores the random number
+    int dice; 
     char choice;
-    srand(time(0)); // "Seed" the random generator
+    srand(time(0)); 
     cout << "\n\t\t========WELCOME TO CASINO WORLD=======\n\n";
     cout << "\n\nWhat's your Name : ";
     getline(cin, playerName);
-    cout << "\n\nEnter the starting balance to play game : $";
+    cout << "\n\nEnter the starting balance to play game : ₹ ";
     cin >> balance;
     do
     {
         system("cls");
         rules();
-        cout << "\n\nYour current balance is $ " << balance << "\n";
-// Get player's betting balance
+        cout << "\n\nYour current balance is ₹ " << balance << "\n";
         do
         {
-            cout << "Hey, " << playerName<<", enter amount to bet : $";
+            cout << "Hey, " << playerName<<", enter amount to bet : ₹";
             cin >> bettingAmount;
             if(bettingAmount > balance)
                 cout << "Betting balance can't be more than current balance!\n"
                        <<"\nRe-enter balance\n";
         }while(bettingAmount > balance);
-// Get player's numbers
         do
         {
             cout << "Guess any betting number between 1 & 10 :";
@@ -49,11 +47,11 @@ int main()
         }
         else
         {
-            cout << "Oops, better luck next time !! You lost $ "<< bettingAmount <<"\n";
+            cout << "Oops, better luck next time !! You lost ₹ "<< bettingAmount <<"\n";
             balance = balance - bettingAmount;
         }
         cout << "\nThe winning number was : " << dice <<"\n";
-        cout << "\n"<<playerName<<", You have balance of $ " << balance << "\n";
+        cout << "\n"<<playerName<<", You have balance of ₹ " << balance << "\n";
         if(balance == 0)
         {
             cout << "You have no money to play ";
@@ -63,7 +61,7 @@ int main()
         cin >> choice;
     }while(choice =='Y'|| choice=='y');
     cout << "\n\n\n";
-    cout << "\n\nThanks for playing the game. Your balance is $ " << balance << "\n\n";
+    cout << "\n\nThanks for playing the game. Your balance is ₹ " << balance << "\n\n";
     return 0;
 }
 void rules()
